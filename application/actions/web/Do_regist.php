ab74132af0234ec3957cd958ddd461d738d17e90
liuwy <liuwy@yindou.com>
2018-01-21 10:40:38 +0800

    内容添加

---
+++ application/actions/web/Do_regist.php
@@ -0,0 +1,26 @@
+<?php                                                                                                                      
+/**
+ *  * @describe:
+ *   * @author: liuwy(liuwy@yindou.com)
+ *    * */
+
+/* vim:set ts=4 sw=4 et fdm=marker: */
+class Do_registAction extends WebBaseAction{
+    public $uid=0;
+    public function beforeExecute(){
+        $this->uid=$this->getUid();
+        if($this->uid){ //已经登录的情况下，就跳到首页吧
+            header("Location:/");
+        }
+    }
+    public function run($args=null){
+        $user = new User();
+        $mobile= $this->getRequestParam('mobile');
+        $pass= $this->getRequestParam('passwd');
+        $ret = $user->addUser($mobile,$pass);
+        if($ret['uid']){
+            $user->createSession($ret['uid']);
+        }
+        var_export($ret);
+    }
+}
